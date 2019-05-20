223ff915995d9005c5ec6d8d86c1672989810b05
JOHNNY DIAS VIANA <04032137@sempreunama.com.br>
2019-05-20 19:45:03 -0300

    Initial commit

---
+++ src/Produtor.java
@@ -0,0 +1,30 @@
+public class Produtor {
+    private String marca;
+    double preço;
+    int codigo;
+
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+
+          public String getMarca () {
+              return marca;
+
+
+              public void setpreço(double preço){
+
+                  this.preço = preço;
+
+
+              }
+
+
+          }
+
+
+
+
+
+
---
+++ src/Supermecador.java
@@ -0,0 +1,7 @@
+public class Supermecador {
+    public static void main (String[]argrs){
+     Produtor p1=new Produtor();
+     p1.preço=2.15;
+     p1.codigo=44444;
+    }
+}
